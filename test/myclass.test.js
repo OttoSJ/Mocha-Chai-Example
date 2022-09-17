db387aacadfb0d8bad54e9bbb6903502f2168e68
Otto Jones <ottosjonesjr@gmail.com>
2022-09-17 10:46:38 -0400

    Added stub and mock test

--- test/myclass.test.js
+++ test/myclass.test.js
@@ -4,25 +4,43 @@ const sinon = require('sinon')
 const chai = require('chai')
 const expect = chai.expect
 
-describe('Test suit', function () {
+describe.skip('Test suit', function () {
   it('Test the add method', function () {
     expect(myObject.add(1, 2)).to.be.equal(3)
   })
 
-  it('Spy the add method', function () {
+  it('spy the add method', function () {
     const spy = sinon.spy(myObject, 'add') // Spy is wrapper around the method you pass it.
-    const callback = sinon.spy()
-
     const arg1 = 10,
-      arg2 = 10
-
+      arg2 = 20
     myObject.callAnotherFn(arg1, arg2)
-
-    myObject.callTheCallback(callback)
-
     // sinon.assert.calledOnce(spy) // <=== One way to do this. See below for another way.
     expect(spy.calledOnce).to.be.true // <=== Another way to do this
     expect(spy.calledWith(arg1, arg2)).to.be.true
+  })
+
+  it('Spy the callback method', function () {
+    const callback = sinon.spy() // <== Creating a spy for callTheCallbace method.
+
+    myObject.callTheCallback(callback) // <== Creates a wrapper for the callback inside callTheCallback method
+
     expect(callback.calledOnce).to.be.true
+  })
+
+  it('Mock the sayHello method', function () {
+    const mock = sinon.mock(myObject)
+    const expectation = mock.expects('sayHello')
+    expectation.exactly(1) // <== Check to verify that callback is called only once
+    expectation.withArgs('hello world') // <=== Check to see if the args passed match the string passed here.
+    myObject.callAnotherFn(10, 20)
+    mock.verify() // <=== Verifies all mock expectations.
+  })
+})
+
+describe('Test suit for stub', function () {
+  it('Stub the add method', function () {
+    const stub = sinon.stub(myObject, 'add')
+    stub.withArgs(10, 20).returns(100)
+    expect(myObject.callAnotherFn(10, 20)).to.be.equal(100)
   })
 })
